99ea0d9fadc7041269b1378a316ba2995b39e4d7
Nymora-World <nymoraworld@gmail.com>
2025-06-16 16:40:16 +0530

    added video section

--- app/dashboard/screens/fourm/page.tsx
+++ app/dashboard/screens/fourm/page.tsx
@@ -1,9 +1,20 @@
-export default function Fourm() {
+import Forum from "../../widgets/Fourm";
+import MainSection from "../../widgets/MainSection";
+import Sidebar from "../../widgets/Sidebar";
+import Task from "../../widgets/Task";
+
+export default function Homework() {
   return (
-    <section className="flex items-center justify-center h-dvh bg-neutral-900">
-      <code className="bg-neutral-800 p-1 rounded-sm text-white">
-        Edit this page
-      </code>
-    </section>
+    <main className="flex h-screen w-full p-0 m-0 overflow-y-hidden">
+      <div className="w-1/5 overflow-y-hidden">
+        <Sidebar />
+      </div>
+      <div className="w-3/5 overflow-y-scroll">
+        <Forum />
+      </div>
+      <div className="w-1/5 bg-gray-50 overflow-y-hidden">
+        <Task />
+      </div>
+    </main>
   );
 }
---
+++ app/dashboard/widgets/Fourm.tsx
@@ -0,0 +1,5 @@
+export default function FourmScreen() {
+    return (
+        <div>Fourm</div>
+    )
+}
