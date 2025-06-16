2eba396cfe4a2b6e0db269907709551807101058
Nymora-World <nymoraworld@gmail.com>
2025-06-16 15:14:01 +0530

    forum

--- app/dashboard/screens/fourm/page.tsx
+++ app/dashboard/screens/fourm/page.tsx
@@ -1,7 +1,9 @@
 export default function Fourm() {
   return (
     <section className="flex items-center justify-center h-dvh bg-neutral-900">
-      <code className="bg-neutral-800 p-1 rounded-sm">Edit this page</code>
+      <code className="bg-neutral-800 p-1 rounded-sm text-white">
+        Edit this page
+      </code>
     </section>
   );
 }
