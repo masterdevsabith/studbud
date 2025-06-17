7432d5f0fac77ba350699f37afcca1f275142206
Nythik Niya <nythikniya@example.com>
2025-06-17 11:42:30 +0530

    Added Hero section with Bento Grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,13 @@
 
 import Hero from "./components/Hero";
 import Navbar from "./components/includes/Navbar";
-
+import {BentoGridDemo} from "./components/ui/bento"
 export default function HomePage() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <BentoGridDemo/>
     </main>
   );
 }
